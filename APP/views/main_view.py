de12ed5d6f28a42aadba258744a7524e9fdaebea
20qqguo <v0888666@gmail.com>
2024-12-15 17:31:18 +0800

    这是最新版的前端

--- APP/views/main_view.py
+++ APP/views/main_view.py
@@ -1,4 +1,5 @@
 import sys
+##前端界面
 
 from PySide6.QtWidgets import QApplication
 from PySide6.QtWidgets import QSpacerItem, QSizePolicy, QComboBox, QHBoxLayout, QMainWindow, QWidget, QVBoxLayout, \
